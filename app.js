875bf2beca9463f98a95cf2a60e3ace05bb2ae30
mohamad-samir <mohamad.samir2011@gmail.com>
2024-04-10 20:49:07 +0200

    get request

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.status(200).json({ message: 'hellow from server side', app: 'Natours' });
 });
 
 app.post('/', (req, res) => {
   res.send('you can post on this endpoint');
+}); */
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
 });
 
 const port = 3000;
