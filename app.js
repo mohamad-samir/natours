d3b0f785afcaacf68cfc55072fdd03576c52c953
mohamad-samir <mohamad.samir2011@gmail.com>
2024-04-11 19:41:01 +0200

    add time middleware

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
     results: tours.length,
