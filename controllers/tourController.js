51fabd4b3708893c550877986ca4c236b93e8cfd
mohamad-samir <mohamad.samir2011@gmail.com>
2024-04-15 23:37:38 +0200

    advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,20 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //build query
+    //1-filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    //2-Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    const query = Tour.find(JSON.parse(queryStr));
+    //Execute query
+    const tours = await query;
+
+    //Send response
     res.status(200).json({
       status: 'success',
       results: tours.length,
