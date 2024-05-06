99895e7e894473f04b817d983898defdfce12f2e
mohamad-samir <mohamad.samir2011@gmail.com>
2024-05-06 20:25:00 +0300

    data sanitize

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,6 +13,8 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1)GLOBAL MIDDLEWARES
+app.use(helmet()); // Apply helmet middleware for enhanced security
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -22,11 +27,23 @@ const limiter = rateLimit({
 
 app.use('/api', limiter); // Apply rate limiting middleware to API routes
 
-app.use(express.json());
+// Middleware to parse incoming requests with JSON payloads
+app.use(express.json({ limit: '10kb' }));
+
+// Middleware to serve static files from the specified directory
 app.use(express.static(`${__dirname}/public`));
 
+// Middleware to sanitize user input against NoSQL query injection
+app.use(mongoSanitize());
+
+// Middleware to sanitize user input against cross-site scripting (XSS) attacks
+app.use(xss());
+
+// Custom middleware function to add a property 'requestTime' to the request object
 app.use((req, res, next) => {
+  // Add a property 'requestTime' to the request object with the current timestamp
   req.requestTime = new Date().toISOString();
+  // Call next() to pass control to the next middleware function
   next();
 });
 
